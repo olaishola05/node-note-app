96699f69e6b49a69e864cefb9216b9d0907b8722
Ola Ishola <olaishola@hotmail.co.uk>
2021-03-09 22:50:02 +0100

    added the db_pass into and env

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
-
+// db connection
+const db_password = process.env.db_password;
+const db_connect = `mongodb+srv://noteUser:${db_password}@notescluster.f21hb.mongodb.net/noteDB?retryWrites=true&w=majority`;
 // middleware
 app.use(morgan("dev"));
 app.use(express.static("public"));
